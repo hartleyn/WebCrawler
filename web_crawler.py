3b01aa9ad79966833bf4b709d292659e3d6f77ee
Nicholas Hartley <nhartley18@me.com>
2017-08-26 10:35:51 -0400

    First commit. Onlt file structure creation implemented.

---
+++ web_crawler.py
@@ -0,0 +1,29 @@
+import os
+
+
+# Each website you crawl is a separate project (folder)
+def create_project_dir(directory):
+	if not os.path.exists(directory):
+		print('Creating project ' + directory)
+		os.makedirs(directory)
+
+create_project_dir('thenewboston')
+
+
+# Create queue and crawled files (if not created)
+def create_data_files(project_name, base_url):
+	queue = project_name + '/queue.txt'
+	crawled = project_name + '/crawled.txt'
+	if not os.path.isfile(queue):
+		write_file(queue, base_url)
+	if not os.path.isfile(crawled):
+		write_file(crawled, '')
+
+
+# Create a new file
+def write_file(path, data):
+	file = open(path, 'w')
+	file.write(data)
+	file.close()
+
+create_data_files('thenewboston', 'https://thenewboston.com/')
